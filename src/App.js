71eda94a0cbc3c84565de8ad9516abbed55fb9d5
Victor Kruze Fiori <kruzefiori@hotmail.com>
2022-08-04 12:20:51 -0300

    mexi no css e no app

--- src/App.js
+++ src/App.js
@@ -3,10 +3,9 @@ import HelpIcon from '@mui/icons-material/Help';
 import Button from '@mui/material/Button';
 
 import './App.css';
-
 const options = []
-for(var i=3; i<=23; i++) {
-  options.push({ value: i.toString(), label:  i.toString() },);
+for (var i = 3; i <= 23; i++) {
+  options.push({ value: i.toString(), label: i.toString() },);
 }
 
 function App() {
@@ -16,31 +15,28 @@ function App() {
         <HelpIcon />
       </header>
       <body>
-        
-        <div className="logo"> 
-          <h1>Vocabuloso</h1>
-        </div>
         <div className="opcoes">
-          <div className="linha">
-            <Select className="numLetras" options={options} />
+        <div className="logo">
+                <h1>Vocabuloso</h1>
+              </div>
+          <div className="custom-select">
             <p>Número de Letras</p>
+            <Select className="numLetras" options={options} />
           </div>
-          <div className="linha">
-            <p>Número de Palavras</p>
+          <div className="custom-select">
+          <p>Número de Palavras</p>
             <Select className="numPalavras" options={options} />
           </div>
-          <div className="linha">
+          <div className="custom-select">
             <p>Número de Tentativas</p>
             <Select className="numTentativas" options={options} />
           </div>
           <div className="buttons">
             <Button variant="contained">Iniciar</Button>
-            <Button variant="contained">Configurações</Button>
           </div>
         </div>
       </body>
       <footer>
-
       </footer>
     </div>
   );
