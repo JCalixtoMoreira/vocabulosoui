1b3b4ac9e94d6a4749777ef189a7e56c0b6a2f81
Victor Kruze Fiori <kruzefiori@hotmail.com>
2022-08-11 16:58:14 -0300

    fazendo css

--- src/components/Help.js
+++ src/components/Help.js
@@ -2,31 +2,47 @@ import React from "react";
 
 import Header from "./Header";
 import Footer from "./Footer";
-
+import './Help.css'
 const Help = () => {
     return (
         <>
             <Header />
             <body>
-                <p>Descubra a palavra misteriosa! Após cada tentativa, as letras mudarão de acordo, mostrando o quão perto você está da solução</p><br/>
-                <p>EX: <strong>COI<span style={{ color: "green" }} >S</span>A</strong></p><br/>
-                <p>Nesse exemplo, a letra O faz parte da palavra e está na posição correta (em verde)</p><br/>
-                <p>EX: <strong>TU<span style={{ color: "yellow" }}>N</span>EL</strong></p><br/>
-                <p>Nesse outro exemplo, a letra N está presente na palavra, porém ela não está na posição correta (em amarelo).</p><br/>
+                <div className="logo">
+                    <div className="square">V</div>
+                    <div className="square">O</div>
+                    <div className="square">C</div>
+                    <div className="square">A</div>
+                    <div className="square">B</div>
+                    <div className="square">U</div>
+                    <div className="square">L</div>
+                    <div className="square">O</div>
+                    <div className="square">S</div>
+                    <div className="square">O</div>
+                </div>
+                <div className="logo">
+                    <div className="square">R</div>
+                    <div className="square">E</div>
+                    <div className="square">G</div>
+                    <div className="square">R</div>
+                    <div className="square">A</div>
+                    <div className="square">S</div>
+                </div>
+                <p>Descubra a palavra misteriosa! Após cada tentativa, as letras mudarão de acordo, mostrando o quão perto você está da solução</p><br />
+                <p>EX: <strong>COI<span style={{ color: "green" }} >S</span>A</strong></p><br />
+                <p>Nesse exemplo, a letra O faz parte da palavra e está na posição correta (em verde)</p><br />
+                <p>EX: <strong>TU<span style={{ color: "darkgoldenrod" }}>N</span>EL</strong></p><br />
+                <p>Nesse outro exemplo, a letra N está presente na palavra, porém ela não está na posição correta (em amarelo).</p><br />
                 <p>Acentos são preenchidos automaticamente em cada tentativa, e as palavras também podem possuir letras repetidas!</p>
                 <ul>
-                    <li>
-                        <p><strong>Número de Letras:</strong>Essa opção define o número de letras que a palavra misteriosa terá, podendo variar de X letras até Y letras. Para iniciantes recomenda-se 4-5 letras</p>
-                    </li>
-                    <li>
-                        <p><strong>Número de Palavras:</strong>Essa opção define o número de palavras que serão jogadas ao mesmo tempo pelo jogador, podendo variar de 1 palavra até Z palavras. Para iniciantes recomenda-se começar com 1 palavra e ir aumentando de acordo.</p>
-                    </li>
-                    <li>
-                        <p><strong>Número de Tentativas:</strong>Essa opção define o número de tentativas que o jogador tem para tentar adivinhar a palavra misteriosa, recomenda-se 4 tentativas a mais do número mínimo necessário (Ex: Para 2 palavras, recomenda-se 6 tentativas, 2+4).</p>
-                    </li>
-                    <li>
-                        <p>Ao selecionar o número de palavras, o número de tentativas será automaticamente modificado para o número recomendado também.</p>
-                    </li>
+                    <p><strong>Número de Letras:</strong> Essa opção define o número de letras que a palavra misteriosa terá, podendo variar de X letras até Y letras. Para iniciantes recomenda-se 4-5 letras</p>
+
+                    <p><strong>Número de Palavras:</strong> Essa opção define o número de palavras que serão jogadas ao mesmo tempo pelo jogador, podendo variar de 1 palavra até Z palavras. Para iniciantes recomenda-se começar com 1 palavra e ir aumentando de acordo.</p>
+
+                    <p><strong>Número de Tentativas:</strong> Essa opção define o número de tentativas que o jogador tem para tentar adivinhar a palavra misteriosa, recomenda-se 4 tentativas a mais do número mínimo necessário (Ex: Para 2 palavras, recomenda-se 6 tentativas, 2+4).</p>
+
+                    <p>Ao selecionar o número de palavras, o número de tentativas será automaticamente modificado para o número recomendado também.</p>
+
                 </ul>
             </body>
             <Footer />
--- src/components/Settings.js
+++ src/components/Settings.js
@@ -24,17 +24,46 @@ const Settings = () => {
         <>
             <Header />
             <body>
-                <h3>Opcoes Adicionais</h3>
-                <FormGroup>
-                    <FormControlLabel control={<Checkbox />} label="Indicar Letras Repetidas" />
-                    <FormControlLabel control={<Checkbox />} label="Permitir Palavras Inexistentes" />
-                    <FormControlLabel control={<Checkbox />} label="Modo Difícil (Apenas para 1 Palavra)" />
-                    <FormControlLabel control={<Checkbox />} label="Modo Sandbox" />
-                </FormGroup>
-                <ModalSettings setColor={setFundo} setIsOpen={setIsOpenFundo} modalIsOpen={modalIsOpenFundo} cor={fundo} text={"Cor de Fundo"}/>
-                <ModalSettings setColor={setLetra} setIsOpen={setIsOpenLetra} modalIsOpen={modalIsOpenLetra} cor={letra} text={"Cor de Letras"}/>
-                <ModalSettings setColor={setAcerto} setIsOpen={setIsOpenAcerto} modalIsOpen={modalIsOpenAcerto} cor={acerto} text={"Cor de Acerto"}/>
-                <ModalSettings setColor={setAcertoPosicao} setIsOpen={setIsOpenAcertoPosicao} modalIsOpen={modalIsOpenAcertoPosicao} cor={acertoPosicao} text={"Cor de Acerto e Posição"}/>
+                <div className="logo">
+                    <div className="square">V</div>
+                    <div className="square">O</div>
+                    <div className="square">C</div>
+                    <div className="square">A</div>
+                    <div className="square">B</div>
+                    <div className="square">U</div>
+                    <div className="square">L</div>
+                    <div className="square">O</div>
+                    <div className="square">S</div>
+                    <div className="square">O</div>
+                </div>
+                <div className="logo">
+                    <div className="square">C</div>
+                    <div className="square">O</div>
+                    <div className="square">N</div>
+                    <div className="square">F</div>
+                    <div className="square">I</div>
+                    <div className="square">G</div>
+                    <div className="square">U</div>
+                    <div className="square">R</div>
+                    <div className="square">A</div>
+                    <div className="square">Ç</div>
+                    <div className="square">Õ</div>
+                    <div className="square">E</div>
+                    <div className="square">S</div>
+                </div>
+                <section>
+                    <FormGroup>
+                        <FormControlLabel className="opcoes" control={<Checkbox />} label="Indicar Letras Repetidas" />
+                        <FormControlLabel className="opcoes" control={<Checkbox />} label="Permitir Palavras Inexistentes" />
+                        <FormControlLabel className="opcoes" control={<Checkbox />} label="Modo Difícil (Apenas para 1 Palavra)" />
+                        <FormControlLabel className="opcoes" control={<Checkbox />} label="Modo Sandbox" />
+                    </FormGroup>
+                    <ModalSettings className='colors' setColor={setFundo} setIsOpen={setIsOpenFundo} modalIsOpen={modalIsOpenFundo} cor={fundo} text={"Cor de Fundo"} />
+                    <ModalSettings className='colors' setColor={setLetra} setIsOpen={setIsOpenLetra} modalIsOpen={modalIsOpenLetra} cor={letra} text={"Cor de Letras"} />
+                    <ModalSettings className='colors' setColor={setAcerto} setIsOpen={setIsOpenAcerto} modalIsOpen={modalIsOpenAcerto} cor={acerto} text={"Cor de Acerto"} />
+                    <ModalSettings className='colors' setColor={setAcertoPosicao} setIsOpen={setIsOpenAcertoPosicao} modalIsOpen={modalIsOpenAcertoPosicao} cor={acertoPosicao} text={"Cor de Acerto e Posição"} />
+                </section>
+
             </body>
             <Footer />
         </>
